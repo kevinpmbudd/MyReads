713358d1befef1106c2b970e79ffe1150f780927
kevinpmbudd <kevin@kevinbudd.com>
2017-08-03 13:50:26 -0500

    Add Book component

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,5 +1,6 @@
  import React, { Component } from 'react'
  import { Link } from 'react-router-dom'
+ import Book from './Book.js'
  import * as BooksAPI from './BooksAPI'
 
  class SearchBooks extends Component {
@@ -45,27 +46,8 @@
         <div className="search-books-results">
           <ol className="books-grid">
           {books.map((book) => (
-              <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 188, 
-                    backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}/>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors[0]}</div>
-              </div>
-            </li>
+            <Book book={book} key={book.id}/>
             ))}
-
           </ol>
         </div>
        </div>
